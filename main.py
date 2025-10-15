fc32c8c24aea7486c067edf878f5f2aab31175b9
hombrefrustrado <hombrefrustrado8@gmail.com>
2025-10-15 21:01:10 +0200

    para lucia

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import LoadData
 import pandas as pd
-prompt = "Muéstrame los 10 primeros pacientes con enfermedad respiratoria en Andalucía"
-df = LoadData.consultaLenguajeNatural(prompt)
+prompt = "SELECT * FROM ENFERMEDAD"
+df = LoadData.realizarConsulta(prompt)
+
+
 print(df.head())
+
+
